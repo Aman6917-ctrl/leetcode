fc514561ba4ed37453a33bd5eda448cd27f714cc
Aman Vasudeo Verma <amanvverma109@gmail.com>
2026-07-07 11:01:40 +0530

    Time: 18 ms (5%), Space: 31.5 MB (21.53%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,5 +1,14 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        for i in range(1,len(arr) - 1):
+            if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+                return i
+# brute force
+# traverse the array from second element to second last element
+# compare current element with left neighbour
+# compare current element with right neighbour
+# if current element is greater than both
+# return current index
         start = 0
         end = len(arr) - 1
         while start <= end:
